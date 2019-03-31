2849bd50a8c7ea03ca8ca3164e322d1fbe298f2c
Juan M. Garcia <garcia.solutions@gmail.com>
2019-03-31 12:14:21 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
+
+const credentials = require('./credentials');
+const bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/config', (req, res) => {
+   MongoClient.connect(credentials.url, { useNewUrlParser: true }, (err, client) => {
+      if (!err) {
+        const collection = client.db(credentials.db).collection('nativeMongo');
+        collection.insertOne(req.body, (err, result) => {
+          if (!err) {
+            res.send(result)
+          } else {
+            res.send('error saving', err)
+          }
+        })
+      } else {
+        res.send('There was an err conecting', err)
+      }
+      client.close();
+    });
+});
+
+app.get('/config/:id', (req, res) => {
+    MongoClient.connect(credentials.url, { useNewUrlParser: true }, (err, client) => {
+        const collection = client.db(credentials.db).collection('nativeMongo');
+        collection.find({_id: ObjectId(req.params.id) }).toArray((err, result) => {
+          if (!err) {
+            res.send(result);
+          } else {
+            res.send('Error querying')
+          }
+          client.close()
+        });
+    });
+});
+
+app.patch('/config/:id', (req, res) => {
+    MongoClient.connect(credentials.url, { useNewUrlParser: true }, (err, client) => {
+        const collection = client.db(credentials.db).collection('nativeMongo');
+        collection.insertOne({test: 'a', test2: 'b'}, (err, result) => {
+          if(!err) {
+            res.send(result);
+          } else {
+            res.send(' Ha habido un error');
+          }
+          client.close()
+        });
+    });
+});
+
+app.listen(3039, () => {
+    console.log('the server has started...')
+});
